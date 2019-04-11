4c3004ef496ea91570d899250f8ae565569062cb
Yawrf <rewilloughby@ymail.com>
2019-04-10 21:19:01 -0400

    Prep for Parent App
    A minor tweak to integrate it into the Parent App

--- src/initiativeholder/InitiativeHolder.java
+++ src/initiativeholder/InitiativeHolder.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package initiative.holder;
+package initiativeholder;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -17,15 +17,21 @@ import javafx.stage.Stage;
  */
 public class InitiativeHolder extends Application {
     
+    private static Scene scene = null;
+    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
         
         stage.setTitle("Initiative Tracker");
         stage.setScene(scene);
         stage.show();
+    }
+    
+    public static Scene getScene() {
+        return scene;
     }
 
     /**
